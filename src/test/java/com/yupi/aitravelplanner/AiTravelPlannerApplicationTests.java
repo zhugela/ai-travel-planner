a49958ba6bbaf3a9daf07a01e98214badad1c311
zhugela <zhugela@users.noreply.github.com>
2026-06-26 07:09:47 +0000

    feat: 复习Ch7笔记
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/test/java/com/yupi/aitravelplanner/AiTravelPlannerApplicationTests.java
+++ src/test/java/com/yupi/aitravelplanner/AiTravelPlannerApplicationTests.java
@@ -1,9 +1,12 @@
 package com.yupi.aitravelplanner;
 
+import com.yupi.aitravelplanner.config.TestMockConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestMockConfig.class)
 class AiTravelPlannerApplicationTests {
 
     @Test
